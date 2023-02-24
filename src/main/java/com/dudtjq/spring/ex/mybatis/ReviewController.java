2b831c76de393d1d1cdbec728e0c1b756a4a016b
DESKTOP-KM2AE57\user <dudtjq>
2023-02-24 18:46:47 +0900

    [이영섭] 1. 스프링 예제 리뷰 파일 수정 하면서 새로운 내용 추가

--- src/main/java/com/dudtjq/spring/ex/mybatis/ReviewController.java
+++ src/main/java/com/dudtjq/spring/ex/mybatis/ReviewController.java
@@ -10,13 +10,14 @@ import com.dudtjq.spring.ex.mybatis.bo.ReviewBO;
 import com.dudtjq.spring.ex.mybatis.model.Review;
 
 @Controller
+@RequestMapping("/mybatis")
 public class ReviewController {
 
 	@Autowired
 	private ReviewBO reviewBO;
 	
 	// 전달받은 id와 일치하는 데이터 조회 그결과를 json으로 response 담기
-	@RequestMapping("/mybatis/ex01")
+	@RequestMapping("/ex01")
 	@ResponseBody
 	public Review review(
 //		@RequestParam("id") int id // 기본 형태
@@ -29,6 +30,26 @@ public class ReviewController {
 		Review review = reviewBO.getReview(id);
 		
 		return review;
+	}
+	
+	// 새로운 리뷰를 등록하는 기능
+	@RequestMapping("/ex02")
+	@ResponseBody
+	public String addReview() {
+		// 4, "콤비네이션피자", "이영섭", 4.5, "할인도 많이 받고 잘 먹었어요" 
+//		int count = reviewBO.addReview(4, "콤비네이션피자", "이영섭", 4.5, "할인도 많이 받고 잘 먹었어요.");
+		
+		Review review = new Review();
+		review.setstoreId(2);
+		review.setMenu("뿌링클");
+		review.setUserName("이영섭");
+		review.setPoint(4.8);
+		review.setReview("맛나용");
+		
+		int count = reviewBO.addReviewByObject(review);
+		
+		// 실행결과 : 1
+		return "실행결과 : " + count;
 	}
 	
 }
--- src/main/java/com/dudtjq/spring/ex/mybatis/dao/ReviewDAO.java
+++ src/main/java/com/dudtjq/spring/ex/mybatis/dao/ReviewDAO.java
@@ -10,4 +10,14 @@ public interface ReviewDAO {
 
 	public Review selectReview(@Param("id") int id);
 	
+	// 리턴 타입은 실행된 행의 갯수로 지정을 해준다
+	public int insertReview(
+			@Param("storeId")	int storeId
+			, @Param("menu") String menu
+			, @Param("userName") String userName
+			, @Param("point") double point
+			, @Param("review") String review);
+	
+	public int insertReviewByObject(Review review);
+	
 }
