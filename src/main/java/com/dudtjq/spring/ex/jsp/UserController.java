0243f4d0ebdbd9a5c8599e1d14c768c53392700e
DESKTOP-KM2AE57\user <dudtjq>
2023-02-27 19:01:54 +0900

    [이영섭] 1. jsp 파일 추가

---
+++ src/main/java/com/dudtjq/spring/ex/jsp/UserController.java
@@ -0,0 +1,59 @@
+package com.dudtjq.spring.ex.jsp;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.dudtjq.spring.ex.jsp.bo.UserBO;
+import com.dudtjq.spring.ex.jsp.model.User;
+
+@Controller
+@RequestMapping("/jsp/user")
+public class UserController {
+	
+	@Autowired
+	private UserBO userBO;
+	
+	// 이름, 생년월일, 이메일, 자기소개 전달받고 데이터 저장
+	// @RequestMapping 기본값으로 get 메소드임
+//	@RequestMapping(path="/add", method=RequestMethod.GET)
+	@PostMapping("/add")
+	@ResponseBody
+	public String addUser(
+				@RequestParam("name") String name
+				, @RequestParam("birthday") String birthday
+				, @RequestParam("email") String email
+				, @RequestParam("introduce") String introduce) {
+		
+	 int count = userBO.addUser(name, birthday, email, introduce);
+		
+		 return "삽입결과 : " + count;
+		 
+	}
+	
+	// jsp 파일 맵핑
+	@GetMapping("/input")
+	public String userInput() {
+		return "jsp/UserInput";
+	}
+	
+	// 가장 최근에 등록된 사용자 정보를 html로 구성 한다.
+	@GetMapping("/lastUser")
+	public String lastUser(Model model) {
+		
+		// 최근 등록된 사용자 조회
+		User lastUser = userBO.getLastUser();
+		
+		// 조회된 결과를 jsp 에서 쓸수 있도록 셋팅
+		model.addAttribute("user", lastUser);
+		
+		return "jsp/lastUser";
+	}
+	
+	
+}
---
+++ src/main/java/com/dudtjq/spring/ex/jsp/model/User.java
@@ -0,0 +1,60 @@
+package com.dudtjq.spring.ex.jsp.model;
+
+import java.util.Date;
+
+public class User {
+
+	private int id;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	private String name;
+	private String brithday;
+	private String email;
+	private String introduce;
+	private Date createdAt;
+	private Date updatedAt;
+	
+
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getBrithday() {
+		return brithday;
+	}
+	public void setBrithday(String brithday) {
+		this.brithday = brithday;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getIntroduce() {
+		return introduce;
+	}
+	public void setIntroduce(String introduce) {
+		this.introduce = introduce;
+	}
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+	
+	
+}
